135eff6626e48fd349fe21e46a81bb1f71da92b0
Asim Amalani <asim.amalani@gmail.com>
2020-01-01 19:27:41 -0500

    appendix2 inheritance, encapsulation example

---
+++ appendix2/Airplane.java
@@ -0,0 +1,18 @@
+/**
+ * Airplane
+ */
+public class Airplane {
+
+    public Airplane() {
+    }
+
+    private int speed;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
---
+++ appendix2/Jet.java
@@ -0,0 +1,20 @@
+/**
+ * Jet
+ */
+public class Jet extends Airplane {
+
+    private static final int MULTIPLIER = 2;
+
+    public Jet() {
+        super();
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        super.setSpeed(speed * MULTIPLIER);
+    }
+
+    public void accelerate() {
+        super.setSpeed(getSpeed() * 2);
+    }
+}

abd72ee4f766133301cf5f3be87adf8e3acb7c79
Asim Amalani <asim.amalani@gmail.com>
2020-01-03 08:36:35 -0500

    c02 test alternate path

--- c02/dogDoors-timer/DogDoorSimulator.java
+++ c02/dogDoors-timer/DogDoorSimulator.java
@@ -6,14 +6,18 @@ public class DogDoorSimulator {
 
     System.out.println("Fido barks to go outside...");
     remote.pressButton();
-
     System.out.println("\nFido has gone outside...");
-    // remote.pressButton();
-
     System.out.println("\nFido's all done...");
-    // remote.pressButton();
 
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+    }
+
+    System.out.println("\n... but he's stuck outside!");
+    System.out.println("\nFido starts barking...");
+    System.out.println("\n... so Gina grabs the remote control");
+    remote.pressButton();
     System.out.println("\nFido's back inside...");
-    // remote.pressButton();
   }
 }
